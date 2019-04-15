5956769b8ccf963ae3dd3ae67def621d545a9b8f
RoryMore <ruairidh.more@gmail.com>
2019-04-15 15:27:12 +1200

    MenuAdded

---
+++ M.E.K.A Defence/Assets/Menu/Scripts/MenuControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ShowControls()
+    {
+       //Enable control canvas
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

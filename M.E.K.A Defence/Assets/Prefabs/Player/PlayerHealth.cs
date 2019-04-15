460412cd2c4e8b32261d337b09a2cc59603e0047
Rory <ruairidh.more@gmail.com>
2019-04-15 23:00:48 +1200

    EnemyPlaceholder
    Enemy greybox added, has health and melee attack, deals damage to greybox player

---
+++ M.E.K.A Defence/Assets/Prefabs/Player/PlayerHealth.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+//Very Important to access UI components!
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    
+    public AudioClip deathClip;
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+
+    bool isDead;
+    bool damaged;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //Damage image lerp goes here if we want one
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        healthSlider.value = currentHealth;
+
+        //That big "oof" plays here
+        //playerAudio.Play();
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        //anim.SetTrigger("Die");
+
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play();
+
+        playerMovement.enabled = false;
+    }
+
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+}

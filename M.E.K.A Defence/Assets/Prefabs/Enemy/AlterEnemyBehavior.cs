343e986ff24f0e7a77d635baa24a0a5668ae59eb
Peter He <hguanqi7@gmail.com>
2019-04-16 16:50:11 +1200

    Player Bullet Test

---
+++ M.E.K.A Defence/Assets/Prefabs/Enemy/AlterEnemyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlterEnemyBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //Keep a range distance between player, face the player, and shoot bullet at player
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ M.E.K.A Defence/Assets/Prefabs/Player/BulletaltShoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletaltShoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bulletprefab;
+    public Transform bulletSpawn;
+    public float bulletspeed;
+    public float lifetime = 3;
+    float timer;
+    float nextfire = 0.4f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.Space) && timer >= nextfire)
+        {
+            nextfire = timer + nextfire;
+            Fire();
+            nextfire = nextfire - timer;
+            timer = 0.0f;
+        }
+        
+    }
+
+    private void Fire()
+    {
+        GameObject bullet = Instantiate(bulletprefab);
+        Physics.IgnoreCollision(bullet.GetComponent<Collider>(), bulletSpawn.parent.GetComponent<Collider>());
+        bullet.transform.position = bulletSpawn.position;
+        Vector3 rotation = bullet.transform.rotation.eulerAngles;
+        
+        bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+        //add force
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletspeed, ForceMode.Impulse);
+        StartCoroutine(BulletDead(bullet, lifetime));
+    }
+
+    private IEnumerator BulletDead(GameObject bullet, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(bullet);
+    }
+}

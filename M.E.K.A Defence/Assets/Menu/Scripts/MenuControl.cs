fa62f333274dd45d276c0e8e426e26d396f8d927
RoryMore <ruairidh.more@gmail.com>
2019-04-18 09:40:30 +1200

    Not required but Louis Asked me to move computers
    So I obliged because no-one can say no to a neko

--- M.E.K.A Defence/Assets/Menu/Scripts/MenuControl.cs
+++ M.E.K.A Defence/Assets/Menu/Scripts/MenuControl.cs
@@ -8,17 +8,27 @@ public class MenuControl : MonoBehaviour
 
     public GameObject panel;
     public GameObject backButton;
+
+    public GameObject creditPanel;
+    public GameObject creditBackButton;
+
     public GameObject mainCanvas;
 
 
     void Start()
     {
+        //Controls
         panel = GameObject.Find("ControlScreen");
         backButton = GameObject.Find("BackButton");
+
+        //Credits
+        creditPanel = GameObject.Find("CreditScreen");
+        creditBackButton = GameObject.Find("BackButton2");
 
         mainCanvas = GameObject.Find("MainMenuScreen");
 
         HideControls();
+        HideCredits();
     }
 
     public void PlayGame()
@@ -48,8 +58,20 @@ public class MenuControl : MonoBehaviour
 
     public void PlayCredits()
     {
+        creditPanel.SetActive(true);
+        creditBackButton.SetActive(true);
 
+        mainCanvas.SetActive(false);
     }
+
+    public void HideCredits()
+    {
+        creditPanel.SetActive(false);
+        creditBackButton.SetActive(false);
+
+        mainCanvas.SetActive(true);
+    }
+
     public void Exit()
     {
         Application.Quit();

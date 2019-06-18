dae0a5b68c6534eb5f8095466244bf36c38265a8
LOuis <louiscresswell33@gmail.com>
2019-06-18 12:29:00 +1200

    going for lunch

---
+++ M.E.K.A Defence/Assets/EnemyLaserBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VolumetricLines;
+
+public class EnemyLaserBall : MonoBehaviour
+{
+    float speed = 25f;
+    float damage = 10f;
+    VolumetricLineBehavior line;
+    float lineWidthBase = 50f;
+
+    private void Awake()
+    {
+        line = GetComponent<VolumetricLineBehavior>();
+        lineWidthBase = line.LineWidth;
+    }
+
+    void Update()
+    {
+        line.LineWidth = lineWidthBase + (lineWidthBase/10f) * Mathf.Sin(Time.time  * 5);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
